c8886aa8ba74342337c57c711fbf0768e451d54b
Teodor Kostadinov <teo@privatenet.uk>
2023-11-04 00:20:34 +0200

    adding django-htmx

--- src/project/settings.py
+++ src/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
